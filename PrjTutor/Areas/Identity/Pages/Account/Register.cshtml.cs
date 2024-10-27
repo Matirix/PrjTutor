ca90314eda7074f72edd66717a5bf1b4a778813f
Matthew <Matirix@live.com>
2024-10-26 18:10:52 -0700

    Fixed ManagerPartial

--- PrjTutor/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PrjTutor/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -127,13 +127,16 @@ namespace PrjTutor.Areas.Identity.Pages.Account
                     Email = Input.Email,
                     CreatedAt = DateTime.Now,
                 };
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
 
+                    await _userManager.AddToRoleAsync(user, "TUTOR");
                     var userId = await _userManager.GetUserIdAsync(user);
+
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                     var callbackUrl = Url.Page(
